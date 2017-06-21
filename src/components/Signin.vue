a27c5e7e2b4b9cf695475ffee11b5c1fa5c4884b
Kaplaugher <kaplaugher94@gmail.com>
2017-06-21 13:58:46 -0400

    added sign up

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,5 +1,38 @@
 <template>
-    <div>
-        Sign In Component
+    <div class="form-inline">
+        <h3>Sign In</h3>
+        <br>
+        <div class="form-group">
+            <input type="text" placeholder="email" class="form-control" v-model="email">
+            <br>
+            <input type="password" placeholder="password" class="form-control" v-model="password">
+            <br>
+            <button class="btn btn-primary" @click="signIn">Sign In</button>
+        </div>
+        <br>
+        <p>{{ error.message }}</p>
+
     </div>
 </template>
+<script>
+    import { firebaseApp } from '../firebaseApp'
+    export default {
+      data() {
+        return {
+          email: '',
+          password: '',
+          error: {
+            message: ''
+          }
+        }
+      },
+      methods: {
+        signIn() {
+          firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+            .catch(error => {
+              this.error = error
+          })
+        }
+      }
+    }
+</script>
